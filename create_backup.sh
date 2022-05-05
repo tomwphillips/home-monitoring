35cbeef5175894903c789bb34ec247ef4572cc94
Tom Phillips <tom@tomwphillips.co.uk>
2022-05-06 00:00:00 +0100

    Add backup script

---
+++ create_backup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -eu
+
+GCLOUD_IMAGE="eu.gcr.io/google.com/cloudsdktool/google-cloud-cli:alpine"
+GCLOUD_CONTAINER="gcloud"
+
+function finish {
+    rm backups/*
+}
+
+trap finish EXIT
+
+docker-compose exec -u "$UID" -e INFLUX_TOKEN="$INFLUX_TOKEN" influxdb influx backup /backups
+
+if [ -z "$(docker ps -q -a -f name=$GCLOUD_CONTAINER)" ]; then
+    docker run --name $GCLOUD_CONTAINER -v ~/gcp_sa_key.json:/key.json \
+        "$GCLOUD_IMAGE" \
+        gcloud auth activate-service-account --key-file key.json
+fi
+
+docker run --rm --volumes-from $GCLOUD_CONTAINER \
+    --mount type=bind,source="/home/pi/home-monitoring/backups",target=/backups \
+    "$GCLOUD_IMAGE" gsutil cp -r "/backups/*" gs://rpi-alpha-backup/
